4cfa703a081eea29d8ebefb9f296c6366383df72
excellence <amitiit.suhag@gmail.com>
2017-06-03 00:30:52 +0530

    removed animation and deleted text headings

--- javascript/my_functions.js
+++ javascript/my_functions.js
@@ -1,18 +1,4 @@
 $(document).ready(function(){
-    $("#togmnt").click(function(){
-    	console.log('click function called');
-    	$("#monthly").show();
-    	$("#yearly").hide();
-    	// $(this).removeClass("monthly").addClass();
-    	// $("#togyr").removeClass().addClass();
-    });
-    $("#togyr").click(function(){
-    	console.log('click function called');
-    	$("#monthly").hide();
-    	$("#yearly").show();
-    	// $(this).removeClass("").addClass("annual");
-    	// $("#togmnt").removeClass().addClass();
-    });
     $('#chk-click').bootstrapToggle({
       on: 'Monthly',
       off: 'Yearly'
